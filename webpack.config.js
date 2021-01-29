9467d2e521b436717361d568d8b695cc643c4b97
arashFiroozabadi <arashfiroozabadii@gmail.com>
2021-01-29 08:45:22 -0500

    fix webpack bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,95 +1,94 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MCEP = require("mini-css-extract-plugin");
+const MCEP = require('mini-css-extract-plugin');
 
 module.exports = {
-	entry: {
-	 index:'./src/index.ts',
-	 login: './src/login/index.ts',
-	},
+  entry: {
+    index: './src/index.ts',
+    login: './src/login/index.ts',
+  },
 
-	mode:"production",
-	
-	devtool: 'inline-source-map',
-	devServer:{
-		contentBase: './dist',
-		hot: true,
-		host: 'localhost',
-		port:3000
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test:/\.scss$/,
-				use:[
-					MCEP.loader,
-					'css-loader', 
-					'sass-loader',
-					"postcss-loader"
-				],
-			},
-			{
+  mode: 'production',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    host: 'localhost',
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MCEP.loader,
+          'css-loader',
+          'sass-loader',
+          'postcss-loader',
+        ],
+      },
+      {
         test: /\.css$/i,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: { importLoaders: 1 },
           },
         ],
       },
-			{
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         use: [
           {
-						loader: 'file-loader',
-						options:{
-							name: 'images/[name].[ext]'
-						}
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]',
+            },
           },
         ],
-			},
-			{
-				// HTML LOADER
-				test: /\.html$/,
-				loader: 'html-loader'
-			},
-		],
-	},
+      },
+      {
+        // HTML LOADER
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+    ],
+  },
 
-	resolve: {
-		extensions: ['.ts', '.js'],
-	},
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
 
-	output: {
-		filename: '[name]/[name].bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: '',
-	},
+  output: {
+    filename: '[name]/[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+  },
 
-	plugins: [
-		new HtmlWebpackPlugin({
-			inject: true,
-			chunks:['index'],
-			filename: 'index.html',
-			template:'src/index.html'
-		}),
-		new HtmlWebpackPlugin({
-			inject: true,
-			chunks:['login','index'],
-			filename: 'login.html',
-			template:'src/login/index.html'
-		}),
-		new MCEP({
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      chunks: ['login', 'index'],
+      filename: 'login/index.html',
+      template: 'src/login/index.html',
+    }),
+    new MCEP({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: "[name]/[name].css",
-      chunkFilename: "[id].css",
+      filename: '[name]/[name].css',
+      chunkFilename: '[id].css',
     }),
-	]
+  ],
 };
